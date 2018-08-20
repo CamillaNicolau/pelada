fc8ce3e1dae076d513fd4ae62b15a25e3b0f3127
millacnicolaugmailcom <millacnicolaugmailcom@a2522c24-941e-11e8-bbf9-23b8cc56e63c>
2018-08-20 21:00:25 +0000

    form de contato

---
+++ web/visualizacoes/contato.php
@@ -0,0 +1,29 @@
+<div id="contato">
+    <div class="panel panel-default ">
+        <div class="panel-heading">Contato</div>
+            <div class="panel-body">	
+                <form class="form-horizontal  mx-auto d-block col-md-6" role="form" action="contato" method="post" name="form_contato" id="form_contato" >
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Nome</label>
+                        <div class="col-md-12">
+                            <input class="form-control" name="name" type="text">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">E-Mail</label>
+                        <div class="col-md-12">
+                            <input class="form-control" name="email" type="email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Mensagem</label>
+                        <div class="col-md-12">
+                            <textarea class="form-control" name="mensagem"></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
