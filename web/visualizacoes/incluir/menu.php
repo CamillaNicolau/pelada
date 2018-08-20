7b13444302a53ab177fd973eb5d27f660ec7191a
millacnicolaugmailcom <millacnicolaugmailcom@a2522c24-941e-11e8-bbf9-23b8cc56e63c>
2018-08-20 00:08:23 +0000

    inclusao do novo painel admin css

--- web/visualizacoes/incluir/menu.php
+++ web/visualizacoes/incluir/menu.php
@@ -1,34 +1,52 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>+Pelada</title>
-    <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap-grid.min.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap-reboot.min.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/css/sweetalert.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/css/complemento.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/css/geral.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/css/menu.css" rel="stylesheet" />
-    <link type="text/css" href="./visualizacoes/fontes/fontawesome-free-5.1.0-web/css/all.css" rel="stylesheet" />
-    <link type="text/css" src="./visualizacoes/css/<?php echo Inicio::getNomePaginaAtual(); ?>.css" rel="stylesheet"/>
-  </head>
-  <body>
-    <div class="container">
-      <div class="navbar navbar-expand-lg  navbar-light">
-        <a class="navbar-brand logo mr-auto" href="#"></a>
-       <button type = "button" class = "navbar-toggler ml-auto" data-toggle= "collapse" data-target= "#menu">
-         <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse ml-auto" id="menu">
-          <ul class="navbar-nav">
-            <li class="nav-item ml-auto"><a class="nav-link grow" href="perfil">Perfil</a></li>
-            <li class="nav-item ml-auto"><a class="nav-link grow" href="pelada">Pelada</a></li>
-            <li class="nav-item ml-auto"><a class="nav-link grow" href="peladeiro">Peladeiro</a></li>
-            <li class="nav-item ml-auto"><a class="nav-link grow" href="#">Contato</a></li>
-            <li class="nav-item ml-auto"><a class="nav-link" href="logout">Sair</a></li>
-          </ul>
-      </div>
-    </div>
-</body>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>+Pelada</title>
+      <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap-grid.min.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/bootstrap/css/bootstrap-reboot.min.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/bootstrap/css/sb-admin.min.css" rel="stylesheet"/>
+      <link type="text/css" href="./visualizacoes/css/sweetalert.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/css/complemento.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/css/geral.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/css/menu.css" rel="stylesheet" />
+      <link type="text/css" href="./visualizacoes/fontes/fontawesome-free-5.1.0-web/css/all.css" rel="stylesheet" />
+      <link type="text/css" src="./visualizacoes/css/<?php echo Inicio::getNomePaginaAtual(); ?>.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+            <a class="navbar-brand mr-1" href="#">Mais Pelada</a>
+            <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+                <i class="fas fa-bars"></i>
+            </button>
+            <ul class="navbar-nav ml-auto ml-md-0">
+                <li class="nav-item dropdown no-arrow mx-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-bell fa-fw"></i>
+                        <span class="badge badge-danger">9+</span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-user-circle fa-fw"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">Configurações</a>
+                        <a class="dropdown-item" href="perfil">Perfil</a>
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout" data-toggle="modal" data-target="#logoutModal">Sair</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <div id="wrapper">
+            <ul class="sidebar navbar-nav">
+                <li><a href="pelada"><i class="fa fa-users"></i> Pelada</a></li>
+                <li><a href="peladeiro"><i class="fa fa-users"></i> Peladeiro</a></li>
+                <li><a href="placar"><i class="fa fa-calendar-o"></i> Placares</a></li>
+                <li><a href="financeiro"><i class="fa fa-dollar"></i> Financeiro</a></li>
+            </ul>
+            <div id="content-wrapper">
+                <div class="container-fluid">   
--- web/visualizacoes/incluir/rodape.php
+++ web/visualizacoes/incluir/rodape.php
@@ -1,5 +1,9 @@
-<p class="mt-5 mb-3 text-muted">&copy;2018</p>
-        </div> <!-- Fecha div .container -->
+
+</div>
+
+<p class="mt-5 mb-3 text-muted">&copy;2018 Mais Pelada</p>
+</div>
+    </div> <!-- Fecha div .container -->
 
         <script type="text/javascript" src="./visualizacoes/js/jquery.min.js" ></script>
         <script type="text/javascript" src="http://malsup.github.com/jquery.form.js"></script>
@@ -7,6 +11,7 @@
         <script type="text/javascript" src="./visualizacoes/js/jquery.maskedinput.js" ></script>
         <script type="text/javascript" src="./visualizacoes/js/sweetalert.min.js" ></script>
         <script type="text/javascript" src="./visualizacoes/js/complemento.js" ></script>
+        <script type="text/javascript" src="./visualizacoes/bootstrap/js/sb-admin.min.js"></script>
         <script type="text/javascript" src="./visualizacoes/js/<?php echo Inicio::getNomePaginaAtual(); ?>.js" ></script>       
     </body>
 </html>
