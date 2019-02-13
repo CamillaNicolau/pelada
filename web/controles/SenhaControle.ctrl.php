e85039cdaaa4d7958b8f84365902fafc25a0bcc2
camillanicolau <camillanicolau@a2522c24-941e-11e8-bbf9-23b8cc56e63c>
2019-02-13 16:28:47 +0000

    atualizaçao

---
+++ web/controles/SenhaControle.ctrl.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Gerencia a exibição da página inicial.
+ *
+ * @author Camilla Nicolau
+ * @version 1.0
+ * @copyright 2018
+ */
+class SenhaControle extends ControlaModelos
+{
+    public function tratarAcoes(){
+      
+        if(isset($_REQUEST['acao']));
+        switch ($_REQUEST['acao']){
+             case 'cadastra_senha':
+                try{
+                    var_dump($_GET);
+                    exit();
+
+                    if ($_POST['password'] != $_POST['passwordConfirm']) {
+                        exit(json_encode(array('sucesso'=>false,'mensagem'=>'As senhas não conferem!')));
+                    }
+
+                    $buscaUsuario = PeladaRepositorio::buscaGeralPelada(['p.token = "'.$_GET['token'].'" and u.ativo ='.true]);
+                    if(count($buscarUsuario)>0){
+                        foreach ($buscarUsuario as $usuario) {
+                            var_dump($usuario);
+                            exit();
+                        }
+                        // \Doctrine::beginTransaction();
+                        // $UsuarioRepositorio = new UsuarioRepositorio();
+                        // $Usuario = new Usuario();
+
+                        // $Usuario->senha = md5($_POST['password']);
+                        // $UsuarioRepositorio->atualizarUsuario($Usuario,false);
+                        // \Doctrine::commit();
+                        // exit(json_encode(array('sucesso'=>true,'mensagem'=>'Senha cadastrada com sucesso')));
+                    }else{
+                        \Doctrine::rollBack();
+                        exit(json_encode(array('sucesso'=>false, 'mensagem'=>'Usuário não cadastrado no sistema, caso queira se cadastrar acesse  <a href="usuario">Cadastro</a>')));
+                    }
+                }catch(Error $E){
+                    \Doctrine::rollBack();
+                    exit(json_encode(array("erro" => true, "sucesso" => false)));
+                }
+            break;
+        }
+    }
+    public function getHtml()
+    {
+        try
+        {
+      
+            /*
+             * Cabeçalho
+             */
+            require PATH_RAIZ . '/visualizacoes/incluir/cabecalho.php';
+
+            /*
+             * Conteúdo
+             */
+            require PATH_RAIZ . '/visualizacoes/senha.php';
+
+            /*
+             * Rodapé
+             */
+            require PATH_RAIZ . '/visualizacoes/incluir/rodape.php';
+            
+        }
+        catch (Exception $ex)
+        {
+            echo 'Exceção: ',  $ex->getMessage(), "\n";
+        }
+		
+    }
+
+}
