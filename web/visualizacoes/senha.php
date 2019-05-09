e998702ea16436164f19aabda54de549912bb08d
camillanicolau <camillanicolau@a2522c24-941e-11e8-bbf9-23b8cc56e63c>
2019-05-09 20:28:56 +0000

    visualização do formulario da senha

--- web/visualizacoes/senha.php
+++ web/visualizacoes/senha.php
@@ -1,12 +1,12 @@
-<div class="border rounded shadow-lg">
-    <div class="cabecalho_conteudo text-center bg-secondary px-3 py-2 rounded-top border-bottom">
+<div class="col-sm-12 col-lg-4 m-auto border rounded shadow-lg bg-white text-center">
+    <div class="row justify-content-center cabecalho_conteudo text-center bg-secondary px-3 py-2 rounded-top border-bottom">
         <div class="box-header">
             <h5 class="mb-0 te">Cadastrar Senha</h5>
         </div>
     </div>
         <form class="form-horizontal mx-auto d-block py-3 col-12 bg-light" action="senha" method="post" name="form_cadastra_senha" id="form_cadastra_senha" >
-            <div class="form-group col-sm-12 col-lg-6">
-                <label for="inputPassword" class="control-label">Informe a nova senha</label>
+            <div class="form-group ">
+                <label for="inputPassword" class="sr-only">Informe a nova senha</label>
                 <div class="col-md-12">
                     <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua Senha">
                     <div class="progress">
@@ -14,14 +14,14 @@
                     </div>
                 </div>
             </div>
-            <div class="form-group col-sm-12 col-lg-6">
-                <label for="inputPassword" class="control-label">Confirme a senha</label>
+            <div class="form-group">
+                <label for="inputPassword" class="sr-only">Confirme a senha</label>
                 <div class="col-md-12">
                     <input type="password" name="passwordConfirm" class="form-control" id="passwordConfirm" placeholder="Confirme a senha">
                 </div>
             </div>
             <input name="acao" value="cadastra_senha" id="acao_usuario" type="hidden" />
-              <button type="submit" class="btn btn-lg btn-success btn-default" id ="botao-cadastrar">Cadastrar</button>
+              <button type="submit" class="btn btn-success btn-default" id ="botao-cadastrar">Cadastrar</button>
         </form>
         
 </div>
