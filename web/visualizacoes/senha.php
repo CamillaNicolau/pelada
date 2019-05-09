5a52d7d6f3f44c27ac36dd8f780338e5bedb3d37
camillanicolau <camillanicolau@a2522c24-941e-11e8-bbf9-23b8cc56e63c>
2019-05-09 20:11:57 +0000

    visualização do formulario da senha

--- web/visualizacoes/senha.php
+++ web/visualizacoes/senha.php
@@ -1,25 +1,27 @@
-<div class=" container-content col h-100 flex-grow-1 mw-100 no-padding bg-secondary">
-    <div class="box-header">
-        <h2 class="box-title py-2 px-4 text-dark shadow"><strong>Cadastrar Senha</strong></h2>
-    </div>
-    <form class="form-horizontal mx-auto d-block col-md-6" action="senha" method="post" name="form_cadastra_senha" id="form_cadastra_senha" >
-        <div class="form-group">
-            <label for="inputPassword" class="col-md-6 control-label">Informe a nova senha</label>
-            <div class="col-md-12">
-                <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua Senha">
-                <div class="progress">
-                    <span id="progresso" class="progress-bar" role="progressbar" aria-valuemax="100"></span>
+<div class="border rounded shadow-lg">
+    <div class="cabecalho_conteudo text-center bg-secondary px-3 py-2 rounded-top border-bottom">
+        <div class="box-header">
+            <h5 class="mb-0 te">Cadastrar Senha</h5>
+        </div>
+        <form class="form-horizontal mx-auto d-block py-3 col-12 bg-light" action="senha" method="post" name="form_cadastra_senha" id="form_cadastra_senha" >
+            <div class="form-group">
+                <label for="inputPassword" class="col-md-6 control-label">Informe a nova senha</label>
+                <div class="col-md-12">
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua Senha">
+                    <div class="progress">
+                        <span id="progresso" class="progress-bar" role="progressbar" aria-valuemax="100"></span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label for="inputPassword" class="col-md-6 control-label">Confirme a senha</label>
-            <div class="col-md-12">
-                <input type="password" name="passwordConfirm" class="form-control" id="passwordConfirm" placeholder="Confirme a senha">
+            <div class="form-group">
+                <label for="inputPassword" class="col-md-6 control-label">Confirme a senha</label>
+                <div class="col-md-12">
+                    <input type="password" name="passwordConfirm" class="form-control" id="passwordConfirm" placeholder="Confirme a senha">
+                </div>
             </div>
-        </div>
-        <input name="acao" value="cadastra_senha" id="acao_usuario" type="hidden" />
-          <button type="submit" class="btn btn-lg btn-success btn-default" id ="botao-cadastrar">Cadastrar</button>
-    </form>
-    
+            <input name="acao" value="cadastra_senha" id="acao_usuario" type="hidden" />
+              <button type="submit" class="btn btn-lg btn-success btn-default" id ="botao-cadastrar">Cadastrar</button>
+        </form>
+        
+    </div>
 </div>
